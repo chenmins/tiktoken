0d05c9df5bdaac777281e9bfce8225d15b123713
chenmin <chenmins@88.com>
2023-11-20 22:46:20 +0800

    提供测试服务

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## 测试说明
+
+```
+curl -X POST http://localhost:8080/encode \
+-H "Content-Type: application/json" \
+-d '{"text":"Hello, world!"}'
+
+```
+ 
--- main.go
+++ main.go
@@ -1,50 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/pkoukk/tiktoken-go"
 	tiktokenloader "github.com/pkoukk/tiktoken-go-loader"
+	"log"
+	"net/http"
 )
 
 func main() {
-	// 如果你不想在运行时下载字典，你可以使用离线加载器
+	// 设置 HTTP 路由处理函数
+	http.HandleFunc("/encode", encodeHandler)
+
+	// 启动 HTTP 服务器
+	log.Println("Server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func encodeHandler(w http.ResponseWriter, r *http.Request) {
+	// 设置响应类型为 JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// 读取请求体中的文本
+	var data struct {
+		Text string `json:"text"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// 原有的代码逻辑
 	tiktoken.SetBpeLoader(tiktokenloader.NewOfflineLoader())
-	text := "Hello, world!"
-	//encoding := "gpt-3.5-turbo"
 	encoding := "gpt-4"
 
 	tkm, err := tiktoken.EncodingForModel(encoding)
 	if err != nil {
-		err = fmt.Errorf("getEncoding: %v", err)
+		http.Error(w, fmt.Sprintf("getEncoding: %v", err), http.StatusInternalServerError)
 		return
 	}
 
 	// encode
-	token := tkm.Encode(text, nil, nil)
+	token := tkm.Encode(data.Text, nil, nil)
 
-	// tokens
-	fmt.Println(token)
-	// num_tokens
-	fmt.Println(len(token))
-}
-
-func main2() {
-	text := "Hello, world!"
-	encoding := "cl100k_base"
-
-	// 如果你不想在运行时下载字典，你可以使用离线加载器
-	tiktoken.SetBpeLoader(tiktokenloader.NewOfflineLoader())
-	tke, err := tiktoken.GetEncoding(encoding)
-	if err != nil {
-		err = fmt.Errorf("getEncoding: %v", err)
-		return
+	// 创建响应
+	response := struct {
+		Tokens    []int `json:"tokens"`
+		NumTokens int   `json:"num_tokens"`
+	}{
+		Tokens:    token,
+		NumTokens: len(token),
 	}
 
-	// encode
-	token := tke.Encode(text, nil, nil)
-
-	//tokens
-	fmt.Println((token))
-	// num_tokens
-	fmt.Println(len(token))
+	// 发送 JSON 响应
+	json.NewEncoder(w).Encode(response)
 }
